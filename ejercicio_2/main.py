c763b7a3bc6858f10780d7b41e945bb95b637bca
Gonzalo Canaza <canazacupegonzalo@gmail.com>
2023-03-03 01:41:53 -0500

    primeros ejercicios

---
+++ ejercicio_2/main.py
@@ -0,0 +1,55 @@
+"""variable = 1
+print(variable)"""
+
+"""x = 1
+if x > 1:
+    print("verdad")
+else: 
+    print("falso")"""
+
+"""x = 1
+if x == 0:
+    print(x)
+elif x == 1:
+    print(x)
+else:
+    print(x)"""
+
+"""name = "jeferson"
+
+for letter in name:
+    print(letter)
+    print("----")
+
+for num in range(5):
+    print(num)
+    print("----")"""
+
+"""x = 0
+while x < 5:
+    print(x)
+    x += 1"""
+
+"""arr = [1, 2, "jeferson"]
+
+for i in arr:
+    print(i)"""
+
+"""arr = [1, 2, 3, 4, 5, 6, 7]
+
+arr.append(14)
+print(arr)
+arr.pop()
+print(arr)
+del arr[-1]
+print(arr)"""
+
+arr = ["peru", "buenos aires"]
+
+capitales = {
+    "peru" : "lima",
+    "argentina" : "buenos aires"
+}
+
+print(arr[0])
+print(capitales["peru"])
